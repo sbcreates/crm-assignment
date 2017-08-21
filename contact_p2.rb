1dc1cbb03a63e9abc1cf23ec518b02fed433b21b
sbcreates <drawsarahb@gmail.com>
2017-08-21 17:10:40 -0400

    Copied contact and crm files. Deleted info from contact. Added active and mini record

--- contact_p2.rb
+++ contact_p2.rb
@@ -1,116 +1,20 @@
+  #transform our Contact class into an ActiveRecord model by inheriting from ActiveRecord::Base.
+
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
+
 class Contact < ActiveRecord::Base
 
-  # This method should initialize the contact's attributes
-  def initialize(first_name, last_name, email, note)
-    @contact_id = (@@unique_id += 1)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-  end
-
-  # This method should call the initializer,
-  # store the newly created contact, and then return it
-  def self.create(first_name, last_name, email, note)
-    new_contact = Contact.new(first_name, last_name, email, note)
-    @@contact_list << new_contact
-    return new_contact
-  end
-
-  # This method should return all of the existing contacts
-  def self.all
-    @@contact_list
-  end
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find(input_id)
-    @@contact_list.each do |id|
-      if input_id == id.contact_id
-        p id.full_name
-      end
-    end
-  end
-
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def print_update
-    p "Attribute to Change"
-    p "[1]  First Name"
-    p "[2]  Last Name"
-    p "[3]  E-mail"
-    p "[4]  Note"
-    p "Enter a number"
-  end
-
-  def update
-    print_update
-    user_input = gets.to_i
-    case user_input
-    when 1
-      puts "Enter First Name"
-      new_first_name = gets.chomp
-      edit_first_name=(new_first_name)
-    when 2
-      puts "Enter Last Name"
-      new_last_name = gets.chomp
-      edit_last_name=(new_last_name)
-    when 3
-      puts "Enter E-mail"
-      new_email = gets.chomp
-      edit_email=(new_email)
-    when 4
-      puts "Enter Note"
-      new_note = gets.chomp
-      edit_note=(new_note)
-    else        exit
-    end
-    p Contact.all
-  end
-
-  def print_find_by
-    p "Search by Attribue"
-    p "[1]  First Name"
-    p "[2]  Last Name"
-    p "[3]  E-mail"
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-    print_find_by
-    user_input = gets.to_i
-    case user_input
-    when 1
-      p "Enter First Name"
-      search_first_name = gets.chomp
-    when 2
-      p "Enter Last Name"
-
-    when 3
-      p "Enter Email"
-    end
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-    @@contact_list.clear
-  end
+  field :first_name, as: :string
+  field :last_name, as: :string
+  field :email, as: :string
+  field :note, as: :string
 
   def full_name
     "#{first_name} #{last_name}"
   end
-
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-    @@contact_list.delete(user)
-  end
-
-  # Feel free to add other methods here, if you need them.
 
 end
--- crm_p2.rb
+++ crm_p2.rb
@@ -22,15 +22,15 @@ class CRM
 
 #Print out the options for the user to choose from in main menu.
   def print_main_menu
-    p "Main Menu"
-    p "*-------*"
-    p "[1]  Add New Contact"
-    p "[2]  Modify Contact"
-    p "[3]  Delete Contact"
-    p "[4]  Display All Contacts"
-    p "[5]  Search by Attribute"
-    p "[6]  Exit"
-    p "Enter a number"
+    puts "Main Menu"
+    puts "*-------*"
+    puts "[1]  Add New Contact"
+    puts "[2]  Modify Contact"
+    puts "[3]  Delete Contact"
+    puts "[4]  Display All Contacts"
+    puts "[5]  Search by Attribute"
+    puts "[6]  Exit"
+    puts "Enter a number"
   end
 
   def call_option(user_menu_input)
@@ -60,13 +60,13 @@ class CRM
         note = "N/A"
       end
     Contact.create(first_name, last_name, email, note)
-    p Contact.all
+    puts Contact.all.inspect
   end
 
 # This method should prompt user to enter an id for the contact to be modified. When id is entered, user is then prompted to select which attribute needs to be changed.
   def modify_existing_contact
     user = {}
-    puts "User ID?"
+    puts "Enter Contact ID"
     search_id = gets.to_i
     Contact.all.each do |person|
       if person.contact_id == search_id
@@ -81,32 +81,41 @@ class CRM
 # This method should prompt user to enter the id of the contact they want to delete.
   def delete_contact
     user = {}
-    puts "User ID?"
+    puts "Enter Contact ID"
     search_id = gets.to_i
     Contact.all.each do |person|
-      if person.contact_id == search_id
-        user = person
+      if search_id == person.contact_id
+        Contact.all.delete(person)
       end
     end
-    puts user
-    delete(user)
+    puts Contact.all.inspect
   end
 
 #This method should show all of the contacts that exist
   def display_all_contacts
-    Contact.all
+    puts Contact.all.inspect
+    main_menu
   end
 
 # This method should prompt user to select whic attribute they want to search by. When they choose, they are prompted to enter the search term. When they enter their search term, they should then be presented with the first contact who matches my search.
   def search_by_attribute
-
+    puts "Search By"
+    puts "[1]  First Name"
+    puts "[2]  Last Name"
+    puts "[3]  E-mail"
+    puts "[4]  Contact ID"
+    puts "Enter a number"
+    user_search_by = gets.to_i
+    case user_search_by
+    when 1 then Contact.find_by(1)
+    when 2 then Contact.find_by(2)
+    when 3 then Contact.find_by(3)
+    when 4 then Contact.find
+    end
   end
 
 end
 
 CRM.new
-p Contact.all
-p Contact.inspect
-Contact.create("A", "B", "C", "N/A")
-Contact.create("B", "C", "D", "N/A")
-Contact.create("C", "D", "E", "N/A")
+puts Contact.all
+puts Contact.inspect
